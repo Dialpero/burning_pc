6d6c209aea32bb5c4d4d93dab1a346837083e752
Diego p <diealperoja@gmail.com>
2020-07-24 17:11:31 -0400

    Se realizo la interfaz de producto

--- app/Http/Controllers/AcademicController.php
+++ app/Http/Controllers/AcademicController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Academic;
 use App\evaluacion;
+use App\departamento;
 use App\Http\Requests\SaveEvaluacionRequest;
 use App\Http\Requests\SaveAcademicRequest;
 use Illuminate\Http\Request;
+use App\Exports\EvaluacionesExport;
+use Maatwebsite\Excel\Facades\Excel; 
+use App\Charts\UserChart;
+use Illuminate\Support\Facades\Auth;
 
 class AcademicController extends Controller
 {
@@ -23,10 +28,25 @@ class AcademicController extends Controller
     public function index()
     {
         //$portafolio = DB::table('academics')->get(); // Sin crear archivo 'academic.php'
-
+        if(Auth::user()->estado == 'activo'){
+            if(Auth::user()->typeuser == 'Secretaria'){ 
+                $id = Auth::user()->id; //Obtiene id de secretario actual logeado
+                $depto = departamento::where('id_Secretaria', $id)->get(); //obtiene el depto de la secretaria
+                $deptoAcademico = $depto[0]['id_Dept'];
+                return view('academics.index', [
+                    'academics' => Academic::where('depto', $deptoAcademico)->latest()->paginate(8) 
+                ]);
+            }
+        }
+        elseif(Auth::user()->typeuser == 'Administrador') {
+            return view('academics.index', [
+                'academics' => Academic::latest()->paginate(8) 
+            ]);
+        }
         return view('academics.index', [
-            'academics' => Academic::latest()->paginate()
+            'academics' => Academic::where('id', 0)->latest()->paginate(8) 
         ]);
+        
     }
 
     public function show($id)
@@ -40,15 +60,48 @@ class AcademicController extends Controller
                 $concatenated = $collection->concat($concatenated)->concat([$evaluacion]);
             }      
         }
+        //Grafico 1
+
+        // Instanciamos el objeto gráfico 
+        $chart = new UserChart;
+
+        // Añadimos las etiquetas del eje X
+        $label = array();
+        $calificaciones = array();
+        foreach($concatenated as $dato){
+            if(empty($concatenated)){
+                break;
+            }
+            array_push($label, $dato->id);
+            array_push($calificaciones, $dato->calificacion_final);
+        }
+        
+        $chart->labels($label);
+        $chart->dataset("Calificación", 'bar', $calificaciones)->backgroundColor('rgba(0,0,255,0.5)');
+
         return view('academics.show',[
             'academic' => Academic::findOrFail($id),
-            'concatenated' => $concatenated
+            'concatenated' => $concatenated,
+            'chart' => $chart
         ]);
     }
     public function create()
     {
+        if(Auth::user()->estado == 'activo'){
+            if(Auth::user()->typeuser == 'Secretaria'){ 
+                $id = Auth::user()->id; //Obtiene id de secretario actual logeado
+                $depto = departamento::where('id_Secretaria', $id)->get(); //obtiene el depto de la secretaria
+                $deptoAcademico = $depto[0]['id_Dept'];
+                return view('academics.create', [
+                    'academic' => new Academic,
+                    'depto_secretario' => $deptoAcademico
+                ]);
+        
+            }
+        }
         return view('academics.create', [
-            'academic' => new Academic
+            'academic' => new Academic,
+            'deptoAcademico' => departamento::get()
         ]);
     }
 
@@ -59,6 +112,18 @@ class AcademicController extends Controller
     }
     public function edit(Academic $academic)
     {
+        if(Auth::user()->estado == 'activo'){
+            if(Auth::user()->typeuser == 'Secretaria'){ 
+                $id = Auth::user()->id; //Obtiene id de secretario actual logeado
+                $depto = departamento::where('id_Secretaria', $id)->get(); //obtiene el depto de la secretaria
+                $deptoAcademico = $depto[0]['id_Dept'];
+                return view('academics.edit', [
+                    'academic' =>  $academic,
+                    'depto_secretario' => $deptoAcademico
+                ]);
+        
+            }
+        }
         return view('academics.edit',[ 
             'academic' =>  $academic
         ]);
@@ -80,5 +145,16 @@ class AcademicController extends Controller
         }
         $academic->delete();
         return redirect()->route('academics.index')->with('status', 'El académico fue eliminado con éxito');  
+    }
+
+    public function export($rut) 
+    {
+        //$resumen = new EvaluacionesExport;
+        //return $resumen->download('pautaResumen.xlsx');
+
+        $resumen = evaluacion::where('rut_academico', '=', $rut)->get();
+ 
+        return (new evaluacionesExport($resumen))->download('pautaResumen.xlsx');
+
     }
 }
--- resources/views/evaluaciones/show.blade.php
+++ resources/views/evaluaciones/show.blade.php
@@ -3,33 +3,73 @@
 @section('tittle', 'Evaluación | ' .$evaluacion->rut_academico) <!-- $academic = elemento de tabla BD academics -->
     
 @section('content')
+
 <div class="container">
     <div class="bg-white p-5 shadow rounded">
         <div class="d-flex justify-content-between align-items-center">
             <h1> {{ $evaluacion-> rut_academico}} </h1>         
         </div>
         <p class="text-secondary"> 
-                ID: {{ $evaluacion->id }} <br> 
+                ID: {{ $evaluacion->id }} <br>
+                Rut evaluador 1: {{ $evaluacion->rut_evaluador1 }} <br>
+                Rut evaluador 2: {{ $evaluacion->rut_evaluador2 }} <br>
                 Calificacion anterior: {{ $evaluacion->calificacion_anterior }}<br>
+                Observación anterior: {{ $evaluacion->observacion_anterior }} <br>
+        </p>
+        <div class="d-flex justify-content-between align-items-left">
+        <p class="text-secondary">
+        --------------------------------------------<br>
+        Actividades de docencia<br>
+        Actividades de de investigación<br>
+        Actividades de Extensión y vinculación<br>
+        Actividades de administración<br>
+        Otras actividades<br>
+        </p>
+        <p class="text-secondary">
+        % de Tiempo<br>
+                {{ $evaluacion->docencia_tiempo }}<br>
+                {{ $evaluacion->investigacion_tiempo }} <br>
+                {{ $evaluacion->extension_tiempo }} <br>
+                {{ $evaluacion->administracion_tiempo }} <br>
+                {{ $evaluacion->otras_tiempo }} <br>
+        </p>
+        <p class="text-secondary">
+        Nota<br>
+                {{ $evaluacion->docencia_nota }} <br>
+                {{ $evaluacion->investigacion_nota }} <br>
+                {{ $evaluacion->extension_nota }} <br>
+                {{ $evaluacion->administracion_nota }} <br>
+                {{ $evaluacion->otras_nota }} <br>
+        </p>
+    
+
+        </div>
+        <p class="text-secondary">
                 Calificacion: {{ $evaluacion->calificacion_final }}<br>
                 Observación: {{ $evaluacion->observacion_final }} <br>
                 Fecha de evaluación: {{ $evaluacion->created_at->format('d/m/Y')}} 
         </p>
         <p class="text-black-50"> {{ $evaluacion->created_at->diffForHumans() }} </p>
         <div class="d-flex justify-content-between align-items-center">
-            <a href="javascript:window.history.back();">Regresar</a>
-            <div class="btn-group btn-group-sm">
-            <a class="btn btn-primary" href="{{ route('evaluaciones.edit', $evaluacion) }}">Editar</a>
-            <a class="btn btn-danger" href="#" onclick="document.getElementById('delete-evaluacion').
-                    submit()">Eliminar</a>
-            </div>
+            <a href="{{ route('academics.index') }}">Regresar</a>
+        @if(Auth::user()->typeuser=='Administrador' or Auth::user()->typeuser=='Secretaria')
+                <div class="btn-group btn-group-sm">
+                <a class="btn btn-primary" href="{{ route('evaluaciones.edit', $evaluacion) }}">Editar</a>
+                <a class="btn btn-danger" href="#" onclick="document.getElementById('delete-evaluacion').
+                        submit()">Eliminar</a>
+                </div>
                 <form id="delete-evaluacion" 
                 class="d-none"
                 method="POST" action="{{ route('evaluaciones.destroy', $evaluacion) }}">
-                    @csrf @method('DELETE')
+                @csrf @method('DELETE')
                 </form>
- 
+
+        @endif
         </div>
+         
+
     </div>
 </div>
+
 @endsection
+ 
